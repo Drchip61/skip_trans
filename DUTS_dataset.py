29d276bd1d0803d092b0ad8ae2ccfad6794268f3
Drchip61 <75516995+Drchip61@users.noreply.github.com>
2021-05-18 16:38:50 +0800

    Add files via upload

--- DUTS_dataset.py
+++ DUTS_dataset.py
@@ -16,6 +16,7 @@ import os
 from PIL import Image
 import numpy as np
 import os
+import cv2
 
 
 
@@ -39,14 +40,29 @@ class MyData(Dataset):
         img = Image.open(img_file)
         label_file = os.path.join(self.train_lb_path,self.train_labels[index])
         labels = Image.open(label_file)
+        #labels = np.array(labels.getdata()).reshape(1,labels.size[0], labels.size[1])
+        labels = self.transform_2(labels)
+        
+        for i in range(labels.size(0)):
+            for j in range(labels.size(1)):
+                for k in range(labels.size(2)):
+                    if labels[i][j][k] >= 0.1:
+                        labels[i][j][k] = 1.0
+                    else:
+                        labels[i][j][k] = 0.0
+                    
         im = self.transform_1(img)
-        lb = self.transform_1(labels)
+        
+        lb = self.transform_3(labels)
+        '''
         for i in range(lb.size(0)):
             for j in range(lb.size(1)):
                 for k in range(lb.size(2)):
                     if lb[i][j][k] >= 0.5:
                         lb[i][j][k] = 1.0
-        lb = lb.squeeze(0)
+        '''
+        #lb = lb[0]
+        
 
         # load label
         #lb = int(self.train_labels[index])
@@ -69,12 +85,46 @@ class MyData(Dataset):
         return im
     
     def transform_1(self,img):
-        transform_img = transforms.Compose([transforms.Resize((22, 22),interpolation = 0),
-                                            transforms.ToTensor()])
+        transform_img = transforms.Compose([transforms.Resize((256, 256)),
+                                            transforms.ToTensor(),
+                                            ])
+        im = transform_img(img)
+        return im
+        
+    def transform_2(self,img):
+        transform_img = transforms.Compose([
+                                            transforms.ToTensor(),
+                                            ])
+        im = transform_img(img)
+        return im
+    def transform_3(self,img):
+        transform_img = transforms.Compose([transforms.Resize((256, 256),interpolation=0)
+                                            
+                                            ])
         im = transform_img(img)
         return im
 
+name = 'E:\PYTHON\DUTS-TR\DUTS-TR\DUTS-TR-Mask\ILSVRC2012_test_00000018.png'
 
 a = MyData()
-print(a[0][0][1].size())
-b = a[0][0][1]
+q,w = a[10]
+q1,q2,q3 = q[0],q[1],q[2]
+print(w.shape)
+q1 = np.array(q1)
+q2 = np.array(q2)
+q3 = np.array(q3)
+w = np.array(w)
+open('check_1.txt','w').write(str(q1.tolist()))
+open('check_2.txt','w').write(str(w.tolist()))
+'''
+img = cv2.merge([w[0]*255])
+cv2.imwrite('q.png',img)
+img1 = cv2.merge([q1*255,q2*255,q3*255])
+cv2.imwrite('w.png',img1)
+'''
+'''
+# 显示图片
+cv2.imshow('test', img)
+cv2.waitKey(0)
+cv2.destroyWindow('test')
+'''
